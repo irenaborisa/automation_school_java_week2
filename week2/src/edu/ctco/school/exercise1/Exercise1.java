68ccbc99ac5151b56aa04c2d095855ef57243b46
irenaborisa <irena.borisa@ctco.lv>
2018-04-04 12:40:20 +0300

    test commit
    Signed-off-by: irenaborisa <irena.borisa@ctco.lv>

--- week2/src/edu/ctco/school/exercise1/Exercise1.java
+++ week2/src/edu/ctco/school/exercise1/Exercise1.java
@@ -4,6 +4,8 @@ package edu.ctco.school.exercise1;
  * Can you find out the error in the below code?
  *
  * How to fix it? What are the options?
+ *
+ * Ans.: top-level class can't be 'private'
  */
 private class Exercise1
 {
